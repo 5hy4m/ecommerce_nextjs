1f9c1e0150ecb24d184ec8532659bfef5d0d2b2c
5hy4m <vcsshyam1998@gmail.com>
2023-03-09 20:32:08 +0530

    Reverted SVG next config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
+        // TODO: Have to change the host name to imageKit.io
         hostname: "media.istockphoto.com",
       },
     ],
