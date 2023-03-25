deb816b2132b8640352991ed26ab317c7bc92b9f
5hy4m <vcsshyam1998@gmail.com>
2023-03-25 15:01:16 +0530

    Added Toys Section

--- src/pages/components/CategorySection/CategorySection.tsx
+++ src/pages/components/CategorySection/CategorySection.tsx
@@ -17,13 +17,21 @@ export default function CategorySection() {
                 </div>
             </div>
             <div className={styles.toys}>
+                <Image
+                    alt=''
+                    src='https://ik.imagekit.io/Hello/foreground_removed.jpg'
+                    placeholder='blur'
+                    blurDataURL='https://ik.imagekit.io/Hello/abstract-blur-green-nature.jpg?bl-10'
+                    fill
+                />
+
                 <div>Toys</div>
-                <div className={styles.image_container}>
+                <div className={styles.toy_image_container}>
                     <Image
                         alt=''
-                        src='https://ik.imagekit.io/Hello/elena-mishlanova-735yzlLr0ls-unsplash_Background_Removed.png?updatedAt=1679723152327'
+                        src='https://ik.imagekit.io/Hello/fullscreen_barbie_Background_Removed.png?updatedAt=1679731652908'
                         placeholder='blur'
-                        blurDataURL='https://ik.imagekit.io/Hello/elena-mishlanova-735yzlLr0ls-unsplash_Background_Removed.png?bl-10'
+                        blurDataURL='https://ik.imagekit.io/Hello/fullscreen_barbie_Background_Removed.png?bl-10'
                         fill
                     />
                 </div>
