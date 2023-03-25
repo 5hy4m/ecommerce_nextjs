3e1b5a9d481748bceb62472d44de26fed8ecac1a
5hy4m <vcsshyam1998@gmail.com>
2023-03-25 12:23:29 +0530

    Added Nextjs config to imagekit

--- next.config.js
+++ next.config.js
@@ -1,15 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        // TODO: Have to change the host name to imageKit.io
-        hostname: "media.istockphoto.com",
-      },
-    ],
-  },
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                // TODO: Have to change the host name to imageKit.io
+                hostname: 'media.istockphoto.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'ik.imagekit.io',
+                // TODO: Have to add pathName to imageKit
+            },
+        ],
+    },
 };
 
 module.exports = nextConfig;
