7d921c7a0de033c14a8ec2061efbb9c208c0d02f
5hy4m <vcsshyam1998@gmail.com>
2023-03-25 19:39:16 +0530

    Category Section Prepared

--- src/pages/components/CategorySection/CategorySection.tsx
+++ src/pages/components/CategorySection/CategorySection.tsx
@@ -5,34 +5,32 @@ export default function CategorySection() {
     return (
         <section className={styles.category_section}>
             <div className={styles.videogames}>
-                <div>Video Games</div>
+                <div className={styles.font}>Video games</div>
+                <a className={styles.anchor}>View all categories</a>
                 <div className={styles.image_container}>
                     <Image
-                        alt=''
-                        src='https://ik.imagekit.io/Hello/Video_Games_Image_fKv1-SGTPi?updatedAt=1679719049704'
-                        placeholder='blur'
-                        blurDataURL='https://ik.imagekit.io/Hello/Video_Games_Image_fKv1-SGTPi?bl-10'
                         fill
+                        alt=''
+                        priority
+                        src='https://ik.imagekit.io/Hello/Video_Games_Image_fKv1-SGTPi?updatedAt=1679719049704'
                     />
                 </div>
             </div>
             <div className={styles.toys}>
                 <Image
-                    alt=''
-                    src='https://ik.imagekit.io/Hello/foreground_removed.jpg'
-                    placeholder='blur'
-                    blurDataURL='https://ik.imagekit.io/Hello/abstract-blur-green-nature.jpg?bl-10'
                     fill
+                    alt=''
+                    priority
+                    src='https://ik.imagekit.io/Hello/foreground_removed.jpg'
                 />
-
-                <div>Toys</div>
+                <div className={styles.font}>Toys</div>
+                <a className={styles.anchor}>View all categories</a>
                 <div className={styles.toy_image_container}>
                     <Image
+                        priority
                         alt=''
-                        src='https://ik.imagekit.io/Hello/fullscreen_barbie_Background_Removed.png?updatedAt=1679731652908'
-                        placeholder='blur'
-                        blurDataURL='https://ik.imagekit.io/Hello/fullscreen_barbie_Background_Removed.png?bl-10'
                         fill
+                        src='https://ik.imagekit.io/Hello/fullscreen_barbie_Background_Removed.png?updatedAt=1679731652908'
                     />
                 </div>
             </div>
