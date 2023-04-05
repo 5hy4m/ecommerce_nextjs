b21e2beb7323e1ce8cf23e5af2f60219d91c7411
5hy4m <vcsshyam1998@gmail.com>
2023-04-05 19:31:39 +0530

    Changed text to we sell

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ export default function Index({ categories }: HomeProps) {
                 <div className={styles.homepage}>
                     <section className={styles.introduction}>
                         <b>
-                            We have <br />
+                            We sell <br />
                         </b>
                         <h1>
                             Retro{' '}
