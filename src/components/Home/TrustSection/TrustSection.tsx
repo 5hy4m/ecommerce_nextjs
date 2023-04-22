c3d34c14d30ca7a66df8aa8b7b771352807f4705
5hy4m <vcsshyam1998@gmail.com>
2023-04-22 12:18:36 +0530

    Cleaned Up Counters

--- src/components/Home/TrustSection/TrustSection.tsx
+++ src/components/Home/TrustSection/TrustSection.tsx
@@ -1,4 +1,4 @@
-import CountUp from 'react-countup';
+import CountUp, { useCountUp } from 'react-countup';
 import ScrollAnimation from 'react-animate-on-scroll';
 import styles from './TrustSection.module.css';
 import {
@@ -10,28 +10,62 @@ import {
 } from '@/components/Icons';
 import Container from 'react-bootstrap/Container';
 import { Counter } from './components/Counter';
+import { useRef } from 'react';
+
+const commonCounterProps = {
+    useEasing: true,
+    duration: 2.75,
+    onStart: ({ pauseResume }: { pauseResume: Function }) => {
+        //Pause Here
+        pauseResume();
+    },
+};
 
 export default function TrustSection() {
+    const sinceCounterRef = useRef(null);
+    const { pauseResume: pauseResumeSinceCounter } = useCountUp({
+        ...commonCounterProps,
+        ref: sinceCounterRef,
+        separator: '',
+        start: 1975,
+        end: 2015,
+    });
+
+    const productCounterRef = useRef(null);
+    const { pauseResume: pauseResumeProductCounter } = useCountUp({
+        ...commonCounterProps,
+        ref: productCounterRef,
+        separator: ',',
+        start: 0,
+        end: 20000,
+    });
+
+    const customerCounterRef = useRef(null);
+    const { pauseResume: pauseResumeCustomerCounter } = useCountUp({
+        ...commonCounterProps,
+        ref: customerCounterRef,
+        separator: ',',
+        start: 0,
+        end: 10000,
+    });
+
     return (
         <section className={styles.trust}>
             <Container className={styles.trust_box_container}>
-                <CountUp separator='' duration={2.75} start={1975} end={2015}>
-                    {({ countUpRef, start }) => (
-                        <h1>
-                            Since
-                            <ScrollAnimation
-                                afterAnimatedIn={(visibility) => {
-                                    if (visibility.inViewport) start();
-                                    return {};
-                                }}
-                                animateIn='fadeInUp'
-                                animateOnce={true}
-                            >
-                                <span ref={countUpRef} />
-                            </ScrollAnimation>
-                        </h1>
-                    )}
-                </CountUp>
+                <h1 onLoad={pauseResumeSinceCounter}>
+                    Since
+                    <ScrollAnimation
+                        afterAnimatedIn={(_) => {
+                            // Resume Here
+                            pauseResumeSinceCounter();
+                            return {};
+                        }}
+                        animateIn='fadeInUp'
+                        animateOnce={true}
+                    >
+                        <span ref={sinceCounterRef} />
+                    </ScrollAnimation>
+                </h1>
 
                 <ScrollAnimation
                     className={styles.service}
@@ -45,57 +79,50 @@ export default function TrustSection() {
                     <WorldIcon height='100px' width='110px' />
                 </ScrollAnimation>
 
-                <CountUp separator=',' duration={2.75} start={0} end={10000}>
-                    {({ countUpRef, start }) => (
-                        <ScrollAnimation
-                            className={styles.products}
-                            animateIn='fadeInUp'
-                            delay={100}
-                            animateOnce={true}
-                            afterAnimatedIn={(visibility) => {
-                                if (visibility.onScreen) start();
-                                return {};
-                            }}
-                        >
-                            <DiagonalArrowIcon height='40px' width='40px' />
+                <ScrollAnimation
+                    className={styles.products}
+                    animateIn='fadeInUp'
+                    delay={100}
+                    initiallyVisible={false}
+                    animateOnce={true}
+                    afterAnimatedIn={(_) => {
+                        pauseResumeProductCounter();
+                        return {};
+                    }}
+                >
+                    <DiagonalArrowIcon height='40px' width='40px' />
 
-                            <div>
-                                PRODUCTS <br />
-                                SOLD
-                            </div>
+                    <div>
+                        PRODUCTS <br />
+                        SOLD
+                    </div>
 
-                            <ProductIcon height='120px' width='120px' />
+                    <ProductIcon height='120px' width='120px' />
 
-                            <Counter countRef={countUpRef} />
-                        </ScrollAnimation>
-                    )}
-                </CountUp>
+                    <Counter countRef={productCounterRef} />
+                </ScrollAnimation>
 
-                <CountUp separator=',' duration={2.75} start={0} end={10000}>
-                    {({ countUpRef, start }) => (
-                        <ScrollAnimation
-                            className={styles.customers}
-                            animateIn='fadeInUp'
-                            delay={200}
-                            animateOnce={true}
-                            afterAnimatedIn={(visibility) => {
-                                if (visibility.inViewport) start();
-                                return {};
-                            }}
-                        >
-                            <HeartRateIcon width='40px' height='40px' />
+                <ScrollAnimation
+                    className={styles.customers}
+                    animateIn='fadeInUp'
+                    delay={200}
+                    animateOnce={true}
+                    afterAnimatedIn={(visibility) => {
+                        pauseResumeCustomerCounter();
+                        return {};
+                    }}
+                >
+                    <HeartRateIcon width='40px' height='40px' />
 
-                            <div>
-                                HAPPY <br />
-                                CUSTOMERS
-                            </div>
+                    <div>
+                        HAPPY <br />
+                        CUSTOMERS
+                    </div>
 
-                            <CustomersIcon width='120px' height='120px' />
+                    <CustomersIcon width='120px' height='120px' />
 
-                            <Counter countRef={countUpRef} />
-                        </ScrollAnimation>
-                    )}
-                </CountUp>
+                    <Counter countRef={customerCounterRef} />
+                </ScrollAnimation>
             </Container>
         </section>
     );
